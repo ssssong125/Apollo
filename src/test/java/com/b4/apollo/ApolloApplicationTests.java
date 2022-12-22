225aae0e6a210bdcf0a7f93d4d7c1ddb54027316
ssssong125 <ssssong125@gmail.com>
2022-12-22 11:09:30 +0900

    Initial commit

---
+++ src/test/java/com/b4/apollo/ApolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b4.apollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
