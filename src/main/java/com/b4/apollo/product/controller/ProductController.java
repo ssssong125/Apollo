7c4638d11c752fa2e0b676c698062d6259c92d4d
ssssong125 <ssssong125@gmail.com>
2022-12-29 18:16:08 +0900

    의존성 추가(타임리프 레이아웃), 헤더 푸터 수정

--- src/main/java/com/b4/apollo/product/controller/ProductController.java
+++ src/main/java/com/b4/apollo/product/controller/ProductController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/product")
+@RequestMapping("product")
 public class ProductController {
     private final ProductService productService;
 
@@ -14,7 +14,7 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @GetMapping("/product")
+    @GetMapping("product")
     public void productList() {
 
     }
