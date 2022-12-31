a799899a637c5bf04952dfbf74dce637fa898b1b
AzuraLight <113497486+AzuraLight@users.noreply.github.com>
2022-12-31 21:32:06 +0900

    22.12.31 21:30 이현도 - 질문 게시판 질문버튼 설정 + 질문 등록 기능 구현 + 질문 상세보기 연결 완료 + 질문 게시판 SQL 수정

--- src/main/java/com/b4/apollo/qna/controller/BoardController.java
+++ src/main/java/com/b4/apollo/qna/controller/BoardController.java
@@ -1,17 +1,22 @@
 package com.b4.apollo.qna.controller;
 
 import com.b4.apollo.common.Pagination;
+import com.b4.apollo.qna.exception.CommonException;
 import com.b4.apollo.qna.model.dto.PageInfo;
 import com.b4.apollo.qna.model.dto.Question;
 import com.b4.apollo.qna.service.BoardService;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 /**
  @FileName : BoardController.java
@@ -33,8 +38,8 @@ public class BoardController {
 
     //질문 게시판 전체 조회
     @RequestMapping("list")
-    public String selectList(@RequestParam(value="currentPage",
-            required = false, defaultValue = "1") int currentPage, Model model ) {
+    public String selectList(@RequestParam(value = "currentPage",
+            required = false, defaultValue = "1") int currentPage, Model model) {
 
         int listCount = boardService.selectListCount();
 
@@ -46,12 +51,32 @@ public class BoardController {
         model.addAttribute("pageInfo", pageInfo);
         return "qna/boardList";
     }
-    
-    //질문 게시판 상세 조회
+
+    @GetMapping("/create")
+    public String insertBoard(){
+        return "qna/board_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String boardTitle
+            , @RequestParam String boardContent) {
+        boardService.insertBoard(boardTitle, boardContent);
+        return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+    }
+
+     //질문 게시판 상세 조회
     @GetMapping(value = "/detail/{bno}")
     public String selectBoard(@PathVariable("bno") int bno, Model model) {
         Question question = boardService.selectBoard(bno);
         model.addAttribute("question", question);
         return "/qna/boardDetail";
+    }
+
+    @RequestMapping("deleteBoard")
+    private String deleteBoard(int bno
+            , HttpServletRequest request) {
+
+        boardService.deleteBoard(bno);
+        return "redirect:listBoard";
     }
 }
--- src/main/java/com/b4/apollo/qna/service/BoardServiceImpl.java
+++ src/main/java/com/b4/apollo/qna/service/BoardServiceImpl.java
@@ -1,5 +1,6 @@
 package com.b4.apollo.qna.service;
 
+import com.b4.apollo.qna.exception.CommonException;
 import com.b4.apollo.qna.exception.DataNotFoundException;
 import com.b4.apollo.qna.model.dao.BoardDao;
 import com.b4.apollo.qna.model.dto.PageInfo;
@@ -8,6 +9,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -24,6 +26,38 @@ public class BoardServiceImpl implements BoardService {
     public int selectListCount() {
         return boardDao.selectListCount(sqlSession);
     }
+
+//    @Override
+//    public void insertBoard(Question q) {
+//        int result = boardDao.insertBoard(q);
+//
+//        if(result < 0) {
+//            throw new CommonException("게시글추가실패 ");
+//        }
+//    }
+
+    @Override
+    public void deleteBoard(int bno) {
+        int result = boardDao.deleteBoard(sqlSession, bno);
+
+        if(result < 0) {
+            throw new CommonException("게시글삭제실패 ");
+        }
+    }
+
+    @Override
+    public void insertBoard(String boardTitle, String boardContent) {
+        Question q = new Question();
+        q.setBoardTitle(boardTitle);
+        q.setBoardContent(boardContent);
+        q.setCreateDate(LocalDateTime.now());
+        int result = boardDao.insertBoard(sqlSession, q);
+
+        if(result < 0) {
+            throw new CommonException("게시글 추가 실패 ");
+        }
+    }
+
 
     @Override
     public ArrayList<Question> selectList(PageInfo pageInfo) {
