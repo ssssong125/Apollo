4f30e335ced6864e91477f1d7e4e40679ad829f2
AidenL <115218707+JihyeongL@users.noreply.github.com>
2023-01-04 09:58:44 +0900

    Mapper로 이름변경

--- src/main/java/com/b4/apollo/common/config/MybatisConfig.java
+++ src/main/java/com/b4/apollo/common/config/MybatisConfig.java
@@ -1,11 +1,15 @@
 package com.b4.apollo.common.config;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
