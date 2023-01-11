282ecc5b62dddfdea90a15a9ba08dc6eb3e76a2c
AidenL <115218707+JihyeongL@users.noreply.github.com>
2023-01-11 13:41:38 +0900

    로그인 수정 회원수정중

--- src/main/java/com/b4/apollo/user/service/UserServiceImpl.java
+++ src/main/java/com/b4/apollo/user/service/UserServiceImpl.java
@@ -6,7 +6,7 @@ import com.b4.apollo.user.model.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -17,10 +17,10 @@ public class UserServiceImpl implements UserService {
     public UserServiceImpl(UserMapper userMapper){this.userMapper = userMapper;}
 
 
-    @Override
-    public List<UserDTO> findAllUser() {
-        return userMapper.findAllUser();
-    }
+//    @Override
+//    public List<UserDTO> findAllUser() {
+//        return userMapper.findAllUser();
+//    }
 
 
 //    @Override
@@ -43,14 +43,15 @@ public class UserServiceImpl implements UserService {
         return result>0 ? true:  false;
     }
 
-    @Override
-    public boolean updateUser(UserDTO updateUTO) throws Exception{
-        int result = userMapper.updateUser(updateUTO);
-        if(result<=0){
-             throw new Exception("메뉴등록 실패");
-        }
-        return result>0 ? true:  false;
-    }
+
+//    @Override
+//    public boolean updateUser(UserDTO updateUTO) throws Exception{
+//        int result = userMapper.updateUser(updateUTO);
+//        if(result<=0){
+//             throw new Exception("회원등록 실패");
+//        }
+//        return result>0 ? true:  false;
+//    }
 
 //    @Override
 //    public int idCheck(String userId) {
@@ -59,13 +60,63 @@ public class UserServiceImpl implements UserService {
 //        return cnt;
 //    }
 
+//        @Override
+//        public void deleteUser(String userId) throws Exception{
+//            int result = userMapper.deleteUser(userId);
+//            if (result < 0) {
+//                throw new Exception("회원탈퇴실패");
+//            }
+//        }
+
     @Override
-    public void deleteUser(String userId) throws Exception{
-            int result = userMapper.deleteUser(userId);
-            if (result < 0) {
-                throw new Exception("회원탈퇴실패");
-            }
+    public boolean loginUser(UserDTO userDTO, HttpSession session) {
+        UserDTO loginUser = userMapper.loginUser(userDTO);
+        boolean result = false;
+        if(loginUser != null){
+            session.setAttribute("loginUser", loginUser);
+            result = true;
         }
+        return result;
+    }
+
+    @Override
+    public UserDTO userDetail(UserDTO userDTO) {
+        return userMapper.userDetail(userDTO);
+    }
+
+    @Override
+    public void updateUser(UserDTO userDTO) {
+        userMapper.updateUser(userDTO);
+    }
+
+    //    @Override
+//    public UserDTO loginUser(UserDTO userDTO) {
+//        userDTO = userMapper.loginUser(userDTO);
+//
+//        return userDTO;
+//    }
+
+//    @Override
+//    public UserDTO showUserInfo(String userId) {
+//        return userMapper.showUserInfo(userId);
+//    }
+//
+//    @Override
+//    public UserDTO findById(Long id) {
+//        return null;
+//    }
+
+    //    @Override
+//    public Map<String, List<String>> getPermitListMap() {
+//        return null;
+//    }
+//    @Override
+//    public void deleteUser(String userId) throws Exception{
+//            int result = userMapper.deleteUser(userId);
+//            if (result < 0) {
+//                throw new Exception("회원탈퇴실패");
+//            }
+//        }
 
 
 }
