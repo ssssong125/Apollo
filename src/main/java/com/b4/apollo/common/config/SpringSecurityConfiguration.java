e4c1ca9063750dc64719651dfabf0f5370fb32c5
AidenL <115218707+JihyeongL@users.noreply.github.com>
2023-01-18 10:45:36 +0900

    23.01.17 18:07

--- src/main/java/com/b4/apollo/common/config/SpringSecurityConfiguration.java
+++ src/main/java/com/b4/apollo/common/config/SpringSecurityConfiguration.java
@@ -14,7 +14,7 @@
 //@EnableWebSecurity
 //@Configuration
 //public class SpringSecurityConfiguration {
-
+//
 //
 //
 //    @Bean
@@ -33,14 +33,8 @@
 //    }
 //    @Bean
 //    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception  {
-////        Map<String, List<String>> permitListMap = authenticationService.getPermitListMap();//getPermitListMap 만들어서 권한별로 접근가능한 url 목록을 받아오자 AuthenticationService, 와 Impl 에 가서 추가구현하자
-////        List<String> adminPermitList = permitListMap.get("adminPermitList");
-////        List<String> memberPermitList = permitListMap.get("memberPermitList");
-////
-////        adminPermitList.forEach(url -> System.out.println("admin permit list : " + url));
-////        memberPermitList.forEach(url -> System.out.println("member permit list : " + url));
 //
-//        http.csrf().disable()  // token 위조방지 비활성화
+//        http.csrf().disable()
 //                .authorizeHttpRequests()
 //                .antMatchers("/","/main","/user/login","user/signup", "/**").permitAll()
 //                .antMatchers("/user/mypage").authenticated()
@@ -52,20 +46,19 @@
 ////                .antMatchers(adminPermitList.toArray(new String[adminPermitList.size()])).hasRole("ADMIN")
 //                .anyRequest().permitAll()
 //                .and()
-//                .formLogin()    //form 을 이용해서 로그인
-//                .loginPage("/user/login")  //
+//                .formLogin()
+//                .loginPage("/user/login")
 //                .loginProcessingUrl("/user/login")
-//                .successForwardUrl("/user/mypage")  //로그인 성공시 이동할 경로 설정
-//                .failureForwardUrl("/user/login") //에러가 발생했을 때 forwarding 할 경로 기술
+//                .successForwardUrl("/user/mypage")
+//                .failureForwardUrl("/user/login")
 //                .and()
-//                .logout() //로그아웃설정
+//                .logout()
 //                .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
-//                .deleteCookies("JSESSIONID") //성공적으로 수행되었을 때 쿠키를 제거할 키 값을 넣어서 설정
-//                .invalidateHttpSession(true) //로그아웃 처리할 때 세션 만료시킴
+//                .deleteCookies("JSESSIONID")
+//                .invalidateHttpSession(true)
 //                .logoutSuccessUrl("/")
 //                .and()
 //                .exceptionHandling();
-////                .accessDeniedPage("/error/denied");
 //
 //        return http.build();
 //    }
